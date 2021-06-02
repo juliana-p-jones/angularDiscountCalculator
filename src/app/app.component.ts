cce63a63c56d303a3f9d6f510244a1716be4916f
juliana jones <juliana.p.jones@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email juliana.p.jones@gmail.com>
2021-06-01 22:08:25 -0400

    commiting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularcalc';
+
+  public num1!: number;
+  public num2!: number;
+
+calculate(){
+  let result: any = this.num1*((100-this.num2)/100);
+  document.getElementById('total')!.innerHTML = result;
 }
+}
+
+
+
+
+
+
